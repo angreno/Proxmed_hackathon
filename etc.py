c3bfd36e2d45c7025f78bcee724b53d611a74338
angreno.18 <110455572+angreno@users.noreply.github.com>
2023-12-10 07:51:54 +0530

    Update etc.py

--- etc.py
+++ etc.py
@@ -1,15 +1,12 @@
 import torch
 
-# create two 3D tensors
 t1 = torch.randn(1.5,1.5,2)
 t2 = torch.randn(1.5,1.5,2)
-
-# dispaly the tensors
 print(t1)
 print("-------------------------------------------")
 print(t2)
 print("-------------------------------------------")
-# concatenate the above tensors
+
 t = torch.cat((t1,t2), 0)
 print(t)
 print(t.shape)
